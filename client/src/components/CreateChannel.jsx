2d8215d9c24c43f521453ab7d32c2ce1b8e6a483
VJS <minsung_96@naver.com>
2021-11-02 23:59:39 +0900

    Container #3

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useChatContext } from 'stream-chat=react'
 
-const CreateChannel = () => {
+import { UserList } from './'
+import { CloseCreateChannel  } from '../assets'
+
+const ChannelNameInput = ({ channelName = '', setChannelName }) => {
+    const handleChange = (e) => {
+        e.preventDefault()
+        setChannelName(e.target.value)
+    }
+
     return (
-        <div>
-            
+        <div className="channel-name-input__wrapper">
+            <p>
+                Name
+            </p>
+            <input value={} onChange={} placeholder="Channel Name(No Spaces Allowed)"/>
+            <p>
+                Add Members
+            </p>
+        </div>
+    )
+}
+
+const CreateChannel = ({ createType, setIsCreating }) => {
+    const [channelName, setChannelName] = useState('')
+
+    return (
+        <div className="create-channel__Container">
+            {/* <ChannelNameInput />*/}
+            <div className="create-channel__header">
+                <p>
+                    {createType === 'team' ? 'Create New Channel' : 'Send your message'}
+                </p>
+                <CloseCreateChannel />
+            </div>
+            {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
         </div>
     )
 }
